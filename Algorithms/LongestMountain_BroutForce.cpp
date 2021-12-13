93f83497ce8bf210eec70a4a41ca91cb6f50b7ac
ferdous-mahmud <ferdous.webdev@gmail.com>
2021-12-13 14:56:08 +0600

    also increase value of i

--- Algorithms/LongestMountain_BroutForce.cpp
+++ Algorithms/LongestMountain_BroutForce.cpp
@@ -1,63 +1,67 @@
 class Solution {
 public:
-  int longestMountain(vector<int> &arr) {
-
-    int result = 0;
-    int size = arr.size();
-
-    int previousRes = -1;
-
-    if (size >= 3) {
-      for (int i = 0; i < size - 2; i++) {
-
-        int front = i;
-        int mid = i + 1;
-        int end = i + 2;
-
-        // peak
-        if (arr[mid] > arr[front] && arr[mid] > arr[end]) {
-
-          // Backup previous value
-          int bFront = front;
-          int bMid = mid;
-          int bEnd = end;
-
-          int backword = 0;
-          int forword = 0;
-
-          while (arr[mid] > arr[front]) {
-            front--;
-            mid--;
-            backword++;
-            if (front < 0) {
-              break;
+    int longestMountain(vector<int>& arr) {
+        
+        int result = 0;
+        int size = arr.size();
+        
+        int previousRes = -1;
+        
+        if(size >= 3){
+            for(int i = 0; i < size -2; i++){
+                
+                    int front = i;
+                    int mid = i + 1;
+                    int end = i + 2;
+                    
+                    //peak
+                    if(arr[mid] > arr[front] && arr[mid] > arr[end]){
+                
+                        // Backup previous value
+                        int bFront = front;
+                        int bMid = mid;
+                        int bEnd = end;
+                        
+                        int backword = 0;
+                        int forword = 0;
+                        
+        
+                        while(arr[mid] > arr[front]){
+                            front --;
+                            mid --;
+                            backword ++;
+                            if(front < 0){
+                                break;
+                            }
+                        }
+                        
+                        front = bFront;
+                        mid = bMid;
+                        end = bEnd;
+                        
+                        while(arr[mid] > arr[end]){
+                            end ++;
+                            mid ++;
+                            forword++;
+                            // Also increase
+                            i++;
+                            if(end > size - 1){
+                                break;
+                            }
+                        }
+                        
+                        result = backword + forword + 1;
+                        
+                        if(previousRes >= result){
+                            result = previousRes;
+                        }
+                        
+                        previousRes = result;
+                    
+                    }
             }
-          }
-
-          front = bFront;
-          mid = bMid;
-          end = bEnd;
-
-          while (arr[mid] > arr[end]) {
-            end++;
-            mid++;
-            forword++;
-            if (end > size - 1) {
-              break;
-            }
-          }
-
-          result = backword + forword + 1;
-
-          if (previousRes >= result) {
-            result = previousRes;
-          }
-
-          previousRes = result;
         }
-      }
+        
+        return result;
     }
-
-    return result;
-  }
 };
